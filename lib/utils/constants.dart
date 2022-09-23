e8cf48c276f201328bd97e2e2954efdb30c0b1f8
Philips Ucheolisah Nge <u.phils.nge@gmail.com>
2022-09-23 16:18:15 -0400

    tutorial starter code  resources and folders

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const double defaultPadding = 16.0;
+const Color priamryColor = Colors.white;
+const Color defaultFontColot = Colors.black87;
+const Color lightFontColot = Colors.white10;
