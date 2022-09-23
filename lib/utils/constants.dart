118546969e4c24a3ea73a8eac947223380d59d21
Philips Ucheolisah Nge <u.phils.nge@gmail.com>
2022-09-23 16:58:55 -0400

    update

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,3 +4,4 @@ const double defaultPadding = 16.0;
 const Color priamryColor = Colors.white;
 const Color defaultFontColot = Colors.black87;
 const Color lightFontColot = Colors.white10;
+const double defaultRadius = 30.0;
